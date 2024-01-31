783d759d47c1addb728f3cda376c66048351759b
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-01-30 21:17:26 -0800

    Removed old files

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,41 @@
 #include "Project.h"
-
-#include <cstdlib>
+#include "FileIO.h"
 
 #include <iostream>
 
 int main(int argc, char* argv[]) {
 
 	
-	std::unique_ptr<Project> proj{ new Project {"newProj3"} };
+	Project proj{"Sup Bitches"};
 
-	proj->startTimer();
+	proj.startTimer();
 
 	std::string i;
 	std::cin >> i;
 
-	proj->endTimer();
+	proj.endTimer();
 
-	proj->startTimer();
+	proj.startTimer();
 	std::cin >> i;
-	proj->endTimer();
+	proj.endTimer();
 
-	proj->startTimer();
+	proj.startTimer();
 	std::cin >> i;
-	proj->endTimer();
+	proj.endTimer();
 
-	std::cout << proj->printAllEntries().str() << "\n\nTotal: " << proj->printTotalTime() << "\n";
+	std::cout << proj.printAllEntries().str() << "\n\nTotal: " << proj.printTotalTime() << "\n";
 
+	std::cout << proj.getLastUpdated().printTime() << std::endl;
+	
+
+	std::string path {"/usr/local/share/projects/Time-Tracker/textfile.txt"};
+	JsonFormat jsonWriter{};
+
+	jsonWriter.write(path, proj);
+
+	Project newProj {jsonWriter.read(path)};
+
+	std::cout << "\n\nAfter read:\n\n" << newProj.printAllEntries().str() << "\n\nTotal: " << newProj.printTotalTime() << "\n";
 
 	
 	return 0;
