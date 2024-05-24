eceef37ec169ecacf25d6014239db35b6ae016f1
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-05-23 17:04:55 -0700

    Setup CMake

---
+++ build/src/CMakeFiles/time-tracker.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/NAS/1-Project-Related/Project-Source-Directories/Time-Tracker/src/Commands.cpp" "src/CMakeFiles/time-tracker.dir/Commands.cpp.o" "gcc" "src/CMakeFiles/time-tracker.dir/Commands.cpp.o.d"
+  "/mnt/NAS/1-Project-Related/Project-Source-Directories/Time-Tracker/src/FileIO.cpp" "src/CMakeFiles/time-tracker.dir/FileIO.cpp.o" "gcc" "src/CMakeFiles/time-tracker.dir/FileIO.cpp.o.d"
+  "/mnt/NAS/1-Project-Related/Project-Source-Directories/Time-Tracker/src/Framework.cpp" "src/CMakeFiles/time-tracker.dir/Framework.cpp.o" "gcc" "src/CMakeFiles/time-tracker.dir/Framework.cpp.o.d"
+  "/mnt/NAS/1-Project-Related/Project-Source-Directories/Time-Tracker/src/Project.cpp" "src/CMakeFiles/time-tracker.dir/Project.cpp.o" "gcc" "src/CMakeFiles/time-tracker.dir/Project.cpp.o.d"
+  "/mnt/NAS/1-Project-Related/Project-Source-Directories/Time-Tracker/src/Settings.cpp" "src/CMakeFiles/time-tracker.dir/Settings.cpp.o" "gcc" "src/CMakeFiles/time-tracker.dir/Settings.cpp.o.d"
+  "/mnt/NAS/1-Project-Related/Project-Source-Directories/Time-Tracker/src/Timing.cpp" "src/CMakeFiles/time-tracker.dir/Timing.cpp.o" "gcc" "src/CMakeFiles/time-tracker.dir/Timing.cpp.o.d"
+  "/mnt/NAS/1-Project-Related/Project-Source-Directories/Time-Tracker/src/main.cpp" "src/CMakeFiles/time-tracker.dir/main.cpp.o" "gcc" "src/CMakeFiles/time-tracker.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
