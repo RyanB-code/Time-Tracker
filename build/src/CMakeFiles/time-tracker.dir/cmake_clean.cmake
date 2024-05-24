9db25919b801ae12ce907ad69fc589ec1a71add2
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-05-23 17:04:55 -0700

    Setup CMake

---
+++ build/src/CMakeFiles/time-tracker.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "../../bin/time-tracker"
+  "../../bin/time-tracker.pdb"
+  "CMakeFiles/time-tracker.dir/Commands.cpp.o"
+  "CMakeFiles/time-tracker.dir/Commands.cpp.o.d"
+  "CMakeFiles/time-tracker.dir/FileIO.cpp.o"
+  "CMakeFiles/time-tracker.dir/FileIO.cpp.o.d"
+  "CMakeFiles/time-tracker.dir/Framework.cpp.o"
+  "CMakeFiles/time-tracker.dir/Framework.cpp.o.d"
+  "CMakeFiles/time-tracker.dir/Project.cpp.o"
+  "CMakeFiles/time-tracker.dir/Project.cpp.o.d"
+  "CMakeFiles/time-tracker.dir/Settings.cpp.o"
+  "CMakeFiles/time-tracker.dir/Settings.cpp.o.d"
+  "CMakeFiles/time-tracker.dir/Timing.cpp.o"
+  "CMakeFiles/time-tracker.dir/Timing.cpp.o.d"
+  "CMakeFiles/time-tracker.dir/main.cpp.o"
+  "CMakeFiles/time-tracker.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/time-tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
