3bd1b8bb5f1880b4fc774a0f53dea3c21ba14701
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2025-03-02 14:11:46 -0800

    v1.7.0
    The .timetrackerrc file can now be found in the ~/.config folder

--- include/Config.h
+++ include/Config.h
@@ -1,3 +1,3 @@
 #define TimeTracker_VERSION_MAJOR 1
-#define TimeTracker_VERSION_MINOR 6
-#define TimeTracker_VERSION_PATCH 5
+#define TimeTracker_VERSION_MINOR 7
+#define TimeTracker_VERSION_PATCH 0
